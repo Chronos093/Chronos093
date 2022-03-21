dbbcb2724fd2d2e5338fb71eed36623b660f7873
Chronos093 <70582239+Chronos093@users.noreply.github.com>
2022-03-21 16:32:04 -0300

    Add files via upload

---
+++ Python/Ex - Calculadora/main.py
@@ -0,0 +1,35 @@
+def soma(a, b):
+    total = a + b
+    return(total)
+
+def sub(a, b):
+    total = a - b
+    return(total)
+    
+def mul(a, b):
+    total = a * b
+    return(total)
+    
+def div(a, b):
+    total = a / b
+    return(total)
+    
+def switch(case):
+    if case == "soma":
+       return(soma(a, b))
+    elif case == "subtração":
+       return(sub(a, b))
+    elif case == "multiplicação":
+       return(mul(a, b))
+    elif case == "divição":
+       return(div(a, b))
+    else:
+       return(print("Operação invalida."))
+           
+
+if __name__ == '__main__':
+    a = int(input('Digite o número: '))
+    b = int(input('Digite o número: '))
+    op = input(f'Qual a operação, soma, subtração, multiplicação, ou divição.')
+    
+    print(f'O resultado é, {switch(case = op)}')
