f714a4515aee90a3343f1d714bcc67f73be46fc1
Chronos093 <70582239+Chronos093@users.noreply.github.com>
2022-03-21 09:50:30 -0300

    Add files via upload

---
+++ Python/Ex - Soma/main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__' :
+    def soma(a, b):
+        result = a + b
+        return(result)
+    
+    a = int(input("Digite o primeiro número: "))
+    b = int(input("Digite o segundo número: "))
+    
+    print(f"O resultado da adição é {soma(a, b)}")
+    
